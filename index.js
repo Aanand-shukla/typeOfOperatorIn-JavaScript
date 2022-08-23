4aa7586dd0744693004fb8e02e412bcac5a091cb
Aanand-shukla <93906271+Aanand-shukla@users.noreply.github.com>
2022-08-23 18:10:45 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+// typeOf operator  in JavaScript
+
+let  myAge ="22"
+ console.log( typeof myAge);
+ myAge = +myAge;
+ console.log(typeof myAge);
+ let myName ="Aanand Shukla";
+  console.log(typeof myName);
+ 
